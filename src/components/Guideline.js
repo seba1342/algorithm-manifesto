69f5fe51b5eac7aae97f80bfd16289c3c1f3b2ed
seba1342 <sbailouni@gmail.com>
2019-10-08 12:28:37 +1100

    Added infinite marquee text to homepage

--- src/components/Guideline.js
+++ src/components/Guideline.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { ReactDOM, useRef, useState, useEffect } from "react";
 import { Link } from "gatsby";
+import Ticker from "react-ticker";
 
-const Guideline = ({ node }) => {
-  const { title } = node.frontmatter;
-  const { slug } = node.fields;
-  return (
-    <Link to={slug}>
-      <h1>{title}</h1>
-    </Link>
-  );
-};
+import "./Guideline.css";
+
+class Guideline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tickerRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // const [hoverRef, isHovered] = useHover();
+    console.log(this.tickerRef.current);
+
+    // if (this.tickerRef.current) {
+    //   this.tickerRef.current.state.elements[0].addEventListener(`click`, this);
+    // }
+  }
+
+  render() {
+    const { title } = this.props.node.frontmatter;
+    const { slug } = this.props.node.fields;
+    return (
+      <Ticker ref={this.tickerRef} move={true} speed={10}>
+        {() => (
+          <Link className={`link`} to={slug}>
+            <div className={`link__item`}>
+              <h1>{title}</h1>
+            </div>
+          </Link>
+        )}
+      </Ticker>
+    );
+  }
+}
 
 export default Guideline;
